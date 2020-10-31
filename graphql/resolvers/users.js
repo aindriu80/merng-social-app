12a2b63ee9d0ba94fd2ea6d6a1ca6635df527fd3
aindriu80 <aindriu80@gmail.com>
2020-10-31 16:29:11 +0000

    Setting up Client & prettier fix

--- graphql/resolvers/users.js
+++ graphql/resolvers/users.js
@@ -2,40 +2,43 @@ const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const { UserInputError } = require("apollo-server");
 
-const {validateRegisterInput,validateLoginInput} = require('../../util/validators')
+const {
+  validateRegisterInput,
+  validateLoginInput,
+} = require("../../util/validators");
 const { SECRET_KEY } = require("../../config");
 const User = require("../../models/User");
 
-function generateToken(user){
+function generateToken(user) {
   return jwt.sign(
-        {
-          id: user.id,
-          email:user.email,
-          username:user.username,
-        },
-        SECRET_KEY,
-        { expiresIn: "1h" }
-      );
-      }
+    {
+      id: user.id,
+      email: user.email,
+      username: user.username,
+    },
+    SECRET_KEY,
+    { expiresIn: "1h" }
+  );
+}
 module.exports = {
   Mutation: {
-    async login(_, {username, password}){      
+    async login(_, { username, password }) {
       const { errors, valid } = validateLoginInput(username, password);
-      const user = await User.findOne({username});
+      const user = await User.findOne({ username });
 
-      if(!valid){
-        throw new UserInputError('Errors', {errors}); 
+      if (!valid) {
+        throw new UserInputError("Errors", { errors });
       }
 
-      if(!user){
-        errors.general ='User not found';
-        throw new UserInputError('User not found', {errors});
-       }
-      
+      if (!user) {
+        errors.general = "User not found";
+        throw new UserInputError("User not found", { errors });
+      }
+
       const match = await bcrypt.compare(password, user.password);
-      if (!match){
-        errors.general = 'Wrong credentials';
-        throw new UserInputError('Wrong credentials', {errors});
+      if (!match) {
+        errors.general = "Wrong credentials";
+        throw new UserInputError("Wrong credentials", { errors });
       }
       const token = generateToken(user);
       return {
@@ -49,9 +52,14 @@ module.exports = {
       { registerInput: { username, email, password, confirmPassword } }
     ) {
       // TODO: Validate user data
-      const {valid, errors} = validateRegisterInput(username, email, password, confirmPassword);
-      if(!valid) {
-        throw new UserInputError('Errors', {errors});
+      const { valid, errors } = validateRegisterInput(
+        username,
+        email,
+        password,
+        confirmPassword
+      );
+      if (!valid) {
+        throw new UserInputError("Errors", { errors });
       }
       // TODO: Make user doesn't already exist
       const user = await User.findOne({ username });
@@ -74,7 +82,7 @@ module.exports = {
       });
       const res = await newUser.save();
 
-      const token = generateToken(res)
+      const token = generateToken(res);
       return {
         ...res._doc,
         id: res._id,
