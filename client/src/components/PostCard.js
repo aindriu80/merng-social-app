34e1c7235059e8514c4592446f687aecc58de58d
aindriu80 <aindriu80@gmail.com>
2020-11-03 20:49:08 +0000

    Displaying Posts

--- client/src/components/PostCard.js
+++ client/src/components/PostCard.js
@@ -1,12 +1,19 @@
 import React from "react";
-import { Card, Icon, Label, Image } from "semantic-ui-react";
+import { Button, Card, Icon, Label, Image } from "semantic-ui-react";
 import moment from "moment";
+import { Link } from "react-router-dom";
 
 function PostCard({
   post: { body, createdAt, id, username, likeCount, commentCount, likes },
 }) {
+  function likePost() {
+    console.log("Like this post!!");
+  }
+  function commentOnPost() {
+    console.log("Comment on this post");
+  }
   return (
-    <Card>
+    <Card fluid>
       <Card.Content>
         <Image
           floated="right"
@@ -14,10 +21,29 @@ function PostCard({
           src="https://react.semantic-ui.com/images/avatar/large/molly.png"
         />
         <Card.Header>{username}</Card.Header>
-        <Card.Meta>{moment(createdAt).fromNow()}</Card.Meta>
+        <Card.Meta as={Link} to={`/posts/${id}`}>
+          {moment(createdAt).fromNow()}
+        </Card.Meta>
         <Card.Description>{body}</Card.Description>
       </Card.Content>
-      <Card.Content extra></Card.Content>
+      <Card.Content extra>
+        <Button as="div" labelPosition="right" onClick={likePost}>
+          <Button color="teal" basic>
+            <Icon name="heart" />
+          </Button>
+          <Label basic color="teal" pointing="left">
+            {likeCount}
+          </Label>
+        </Button>
+        <Button as="div" labelPosition="right" onClick={commentOnPost}>
+          <Button color="blue" basic>
+            <Icon name="comments" />
+          </Button>
+          <Label basic color="blue" pointing="left">
+            {commentCount}
+          </Label>
+        </Button>
+      </Card.Content>
     </Card>
   );
 }
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -11,13 +11,9 @@ function Home() {
     data: { getPosts: posts },
   } = useQuery(FETCH_POSTS_QUERY);
 
-  // if (data) {
-  //   console.log("data", data);
-  // }
-
   return (
     <Grid columns={3}>
-      <Grid.Row>
+      <Grid.Row className="page-title">
         <h1>Recent Posts</h1>
       </Grid.Row>
       <Grid.Row>
@@ -26,7 +22,7 @@ function Home() {
         ) : (
           posts &&
           posts.map((post) => (
-            <Grid.Column key={post.id}>
+            <Grid.Column key={post.id} style={{ marginBottom: 20 }}>
               <PostCard post={post} />
             </Grid.Column>
           ))
