432629b051650d7db0e430c6660080166c7ca0dd
aindriu80 <aindriu80@gmail.com>
2020-11-04 22:17:02 +0000

    Login & Register Pages & apollo loading data fix

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -6,10 +6,9 @@ import { Grid } from "semantic-ui-react";
 import PostCard from "../components/PostCard";
 
 function Home() {
-  const {
-    loading,
-    data: { getPosts: posts },
-  } = useQuery(FETCH_POSTS_QUERY);
+  const { loading, data = {} } = useQuery(FETCH_POSTS_QUERY);
+
+  const posts = data.getPosts;
 
   return (
     <Grid columns={3}>
